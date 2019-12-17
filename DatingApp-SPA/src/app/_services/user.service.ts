2d0350446277c14ebda6b7560f2099d7d74c89a2
Thomas Mitchell <thomas.mitchell@boomworks.com.au>
2019-12-17 14:24:00 +1100

    End of section 16

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -9,101 +9,144 @@ import { PaginatedResult } from '../_models/pagination';
 import { Message } from '../_models/message';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
-  baseUrl = environment.apiUrl;
+    baseUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {}
 
-  getUsers(page?, itemsPerPage?, userParams?, likesParam?): Observable<PaginatedResult<User[]>> {
-    const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
+    getUsers(
+        page?,
+        itemsPerPage?,
+        userParams?,
+        likesParam?
+    ): Observable<PaginatedResult<User[]>> {
+        const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<
+            User[]
+        >();
 
-    let params = new HttpParams();
+        let params = new HttpParams();
 
-    if (page != null && itemsPerPage != null) {
-      params = params.append('pageNumber', page);
-      params = params.append('pageSize', itemsPerPage);
-    }
-
-    if (userParams != null) {
-      params = params.append('minAge', userParams.minAge);
-      params = params.append('maxAge', userParams.maxAge);
-      params = params.append('gender', userParams.gender);
-      params = params.append('orderBy', userParams.orderBy);
-    }
-
-    if (likesParam === 'Likers') {
-      params = params.append('likers', 'true');
-    }
-
-    if (likesParam === 'Likees') {
-      params = params.append('likees', 'true');
-    }
-
-    return this.http.get<User[]>(this.baseUrl + 'users', {
-      observe: 'response',
-      params
-    }).pipe(
-      map(response => {
-        paginatedResult.result = response.body;
-        if (response.headers.get('Pagination') !== null) {
-          paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+        if (page != null && itemsPerPage != null) {
+            params = params.append('pageNumber', page);
+            params = params.append('pageSize', itemsPerPage);
         }
-        return paginatedResult;
-      })
-    );
-  }
 
-  getUser(id: number): Observable<User> {
-    return this.http.get<User>(this.baseUrl + 'users/' + id);
-  }
+        if (userParams != null) {
+            params = params.append('minAge', userParams.minAge);
+            params = params.append('maxAge', userParams.maxAge);
+            params = params.append('gender', userParams.gender);
+            params = params.append('orderBy', userParams.orderBy);
+        }
 
-  updateUser(id: number, user: User) {
-    return this.http.put(this.baseUrl + 'users/' + id, user);
-  }
+        if (likesParam === 'Likers') {
+            params = params.append('likers', 'true');
+        }
 
-  setMainPhoto(userId: number, id: number) {
-    return this.http.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain', {});
-  }
+        if (likesParam === 'Likees') {
+            params = params.append('likees', 'true');
+        }
 
-  deletePhoto(userId: number, id: number) {
-    return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
-  }
-
-  sendLike(id: number, recipientId: number) {
-    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
-  }
-
-  getMessages(id: number, page?, itemsPerPage?, messageContainer?) {
-    const paginatedResult: PaginatedResult<Message[]> = new PaginatedResult<Message[]>();
-
-    let params = new HttpParams();
-    params = params.append('MessageContainer', messageContainer);
-
-    if (page != null && itemsPerPage != null) {
-      params = params.append('pageNumber', page);
-      params = params.append('pageSize', itemsPerPage);
+        return this.http
+            .get<User[]>(this.baseUrl + 'users', {
+                observe: 'response',
+                params
+            })
+            .pipe(
+                map(response => {
+                    paginatedResult.result = response.body;
+                    if (response.headers.get('Pagination') !== null) {
+                        paginatedResult.pagination = JSON.parse(
+                            response.headers.get('Pagination')
+                        );
+                    }
+                    return paginatedResult;
+                })
+            );
     }
 
-    return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages', {observe: 'response', params})
-      .pipe(
-        map(response => {
-          paginatedResult.result = response.body;
-          if (response.headers.get('Pagination') !== null) {
-            paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
-          }
+    getUser(id: number): Observable<User> {
+        return this.http.get<User>(this.baseUrl + 'users/' + id);
+    }
 
-          return paginatedResult;
-        })
-      );
-  }
+    updateUser(id: number, user: User) {
+        return this.http.put(this.baseUrl + 'users/' + id, user);
+    }
 
-  getMessageThread(id: number, recipientId: number) {
-    return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId);
-  }
+    setMainPhoto(userId: number, id: number) {
+        return this.http.post(
+            this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain',
+            {}
+        );
+    }
 
-  sendMessage(id: number, message: Message) {
-      return this.http.post(this.baseUrl + 'users/' + id + '/messages', message);
-  }
+    deletePhoto(userId: number, id: number) {
+        return this.http.delete(
+            this.baseUrl + 'users/' + userId + '/photos/' + id
+        );
+    }
+
+    sendLike(id: number, recipientId: number) {
+        return this.http.post(
+            this.baseUrl + 'users/' + id + '/like/' + recipientId,
+            {}
+        );
+    }
+
+    getMessages(id: number, page?, itemsPerPage?, messageContainer?) {
+        const paginatedResult: PaginatedResult<Message[]> = new PaginatedResult<
+            Message[]
+        >();
+
+        let params = new HttpParams();
+        params = params.append('MessageContainer', messageContainer);
+
+        if (page != null && itemsPerPage != null) {
+            params = params.append('pageNumber', page);
+            params = params.append('pageSize', itemsPerPage);
+        }
+
+        return this.http
+            .get<Message[]>(this.baseUrl + 'users/' + id + '/messages', {
+                observe: 'response',
+                params
+            })
+            .pipe(
+                map(response => {
+                    paginatedResult.result = response.body;
+                    if (response.headers.get('Pagination') !== null) {
+                        paginatedResult.pagination = JSON.parse(
+                            response.headers.get('Pagination')
+                        );
+                    }
+
+                    return paginatedResult;
+                })
+            );
+    }
+
+    getMessageThread(id: number, recipientId: number) {
+        return this.http.get<Message[]>(
+            this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId
+        );
+    }
+
+    sendMessage(id: number, message: Message) {
+        return this.http.post(
+            this.baseUrl + 'users/' + id + '/messages',
+            message
+        );
+    }
+
+    deleteMessage(id: number, userId: number) {
+        return this.http.post(
+            this.baseUrl + 'users/' + userId + '/messages/' + id,
+            {}
+        );
+    }
+
+    markAsRead(userId: number, messageId: number) {
+        this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + messageId + '/read', {}).subscribe();
+    }
 }
--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -36,7 +36,8 @@ namespace DatingApp.API.Controllers
 
             var messageFromRepo = await _repo.GetMessage(id);
 
-            if(messageFromRepo == null) {
+            if (messageFromRepo == null)
+            {
                 return NotFound();
             }
 
@@ -94,7 +95,8 @@ namespace DatingApp.API.Controllers
 
             var recipient = await _repo.GetUser(messageForCreationDto.RecipientId);
 
-            if(recipient == null) {
+            if (recipient == null)
+            {
                 return BadRequest("Could not find user");
             }
 
@@ -102,12 +104,68 @@ namespace DatingApp.API.Controllers
 
             _repo.Add(message);
 
-            if(await _repo.SaveAll()) {
+            if (await _repo.SaveAll())
+            {
                 var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
-                return CreatedAtRoute("GetMessage", new {id = message.Id}, messageToReturn);
+                return CreatedAtRoute("GetMessage", new { id = message.Id }, messageToReturn);
             }
 
             throw new Exception("Creating the message failed on save");
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> DeleteMessage(int id, int userId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var messageFromRepo = await _repo.GetMessage(id);
+
+            if (messageFromRepo.SenderId == userId)
+            {
+                messageFromRepo.SenderDeleted = true;
+            }
+
+            if (messageFromRepo.RecipientId == userId)
+            {
+                messageFromRepo.RecipientDeleted = true;
+            }
+
+            if (messageFromRepo.SenderDeleted && messageFromRepo.RecipientDeleted)
+            {
+                _repo.Delete(messageFromRepo);
+            }
+
+            if (await _repo.SaveAll())
+            {
+                return NoContent();
+            }
+
+            throw new Exception("Error deleting the mesage");
+        }
+
+        [HttpPost("{id}/read")]
+        public async Task<IActionResult> MarkMessageAsRead(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var message = await _repo.GetMessage(id);
+
+            if(message.RecipientId != userId) {
+                return Unauthorized();
+            }
+
+            message.IsRead = true;
+            message.DateRead = DateTime.Now;
+
+            await _repo.SaveAll();
+
+            return NoContent();
         }
     }
 }
